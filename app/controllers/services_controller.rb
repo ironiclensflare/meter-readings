1f54f4147cb463b59293cb5c4d22605a901d0a1c
Stuart Carruthers <stu@ironiclensflare.com>
2016-02-14 15:14:08 +0000

    Started on graph

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,45 @@
 class ServicesController < ApplicationController
 	def index
 		@services = Service.all
+
+		@graph_size = {
+			:height => 300,
+			:width => 600
+		}
+
+		graph_data = {
+			labels: ['1', '2', '3'],
+			datasets: []
+		}
+
+		@services.each do |service|
+			data = []
+			service.readings.last(3).each do |reading|
+				data.push(reading.value)
+			end
+
+			graph_data[:datasets].push({
+				label: service.name,
+				fillColor: 'rgba(128,128,128,0.5)',
+				data: data
+			})
+		end
+
+		@data = graph_data.to_json
+	end
+
+	def new
+		@service = Service.new
+	end
+
+	def create
+		service = Service.new(service_params)
+		service.save
+		redirect_to root_path
+	end
+
+	private
+	def service_params
+		params.require(:service).permit(:name, :icon)
 	end
 end
